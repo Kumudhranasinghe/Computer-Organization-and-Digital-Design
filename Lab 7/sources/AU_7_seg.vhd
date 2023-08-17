ee1887326e57aa047bd51672170e17fa7c1ba281
Kumudh Ranasinghe <107451577+k-ranasinghe@users.noreply.github.com>
2023-08-17 07:43:17 +0530

    Create AU_7_seg.vhd

---
+++ Lab 7/sources/AU_7_seg.vhd
@@ -0,0 +1,76 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 04/28/2023 08:28:35 PM
+-- Design Name: 
+-- Module Name: AU_7_seg - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity AU_7_seg is
+    Port ( A : in STD_LOGIC_VECTOR (3 downto 0);
+           Clk : in STD_LOGIC;
+           RegSel : in STD_LOGIC;
+           S_LED : inout STD_LOGIC_VECTOR (3 downto 0);
+           S_7Seg : out STD_LOGIC_VECTOR (6 downto 0);
+           Carry : out STD_LOGIC;
+           Zero : out STD_LOGIC);
+end AU_7_seg;
+
+architecture Behavioral of AU_7_seg is
+    component AU is
+    Port ( A : in STD_LOGIC_VECTOR (3 downto 0);
+           RegSel : in STD_LOGIC;
+           Clk : in STD_LOGIC;
+           S : out STD_LOGIC_VECTOR (3 downto 0);
+           Zero : out STD_LOGIC;
+           Carry : out STD_LOGIC);
+    end component;
+    
+    component LUT_16_7 is
+    Port ( address : in STD_LOGIC_VECTOR (3 downto 0);
+           data : out STD_LOGIC_VECTOR (6 downto 0));
+    end component;
+
+begin
+    AU_0 : AU
+    port map(
+        A => A,
+        RegSel => RegSel,
+        Clk => Clk,
+        S => S_LED,
+        Zero => Zero,
+        Carry => Carry );
+        
+
+    
+    LUT_16_7_0 : LUT_16_7
+    port map(
+        address => S_LED,
+        data => S_7Seg );
+
+end Behavioral;
