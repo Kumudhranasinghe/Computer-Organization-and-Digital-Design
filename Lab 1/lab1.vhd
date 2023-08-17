85eea3e65f353918f6e19066cfaf578edf8f5342
Kumudh Ranasinghe <107451577+k-ranasinghe@users.noreply.github.com>
2023-08-17 07:10:44 +0530

    Create lab1.vhd

---
+++ Lab 1/lab1.vhd
@@ -0,0 +1,48 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 03/07/2023 11:13:29 AM
+-- Design Name: 
+-- Module Name: lab1 - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity lab1 is
+    Port ( A : in STD_LOGIC;
+           B : in STD_LOGIC;
+           C : in STD_LOGIC;
+           X : out STD_LOGIC);
+end lab1;
+
+architecture Behavioral of lab1 is
+signal AND_out_sig : std_logic;
+
+begin
+    AND_out_sig <= A AND B;
+    X <= AND_out_sig OR C;
+
+end Behavioral;
